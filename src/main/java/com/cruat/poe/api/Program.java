1b7a1b4155753e82ebe64038035a4f9c585417c2
Joseph <aelphaeis@live.com>
2021-05-29 17:49:05 -0400

    #1 initial commit.

---
+++ src/main/java/com/cruat/poe/api/Program.java
@@ -0,0 +1,11 @@
+package com.cruat.poe.api;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Program {
+	private static final Logger logger = LogManager.getLogger();
+	public static void main(String[] args) {
+		logger.info("Hello World");
+	}
+}
---
+++ src/test/java/com/cruat/poe/api/ProgramTest.java
@@ -0,0 +1,12 @@
+package com.cruat.poe.api;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class ProgramTest {
+
+	@Test
+	public void testMain() {
+		assertTrue(true);
+	}
+}
